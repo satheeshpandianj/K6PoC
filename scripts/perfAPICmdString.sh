0b2a975260a0cd8ed942c7cea83df532a4d2a569
satheeshpandianj <52313344+satheeshpandianj@users.noreply.github.com>
2021-08-30 17:40:45 +0530

    Added scripts folder

---
+++ scripts/perfAPICmdString.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+ENV=$1
+PROJECT=$2
+APINAME=$3
+DATA=$4
+USERS=$5
+DURATION=$6
+DBNAME=$7
+#####################################################################################
+##                            PARSE API NAMES TO USE                           ##
+#####################################################################################
+IFS=',' read -ra API_FILES <<<"$APINAME"
+    echo ${API_FILES[*]}
+#####################################################################################
+##                                 EXECUTE TESTS                                   ##
+#####################################################################################
+for apiName in "${API_FILES[@]}";do
+	k6_test='k6 run -e ENV='$ENV' -e Project='$PROJECT' -e APINAME='$apiName' -e DATA='$DATA' -e USERS='$USERS' -e TESTINGTIME='$DURATION' --out influxdb=http://104.40.213.24:8086/'$DBNAME' ./scripts/perfAPITestScript.js'
+	$k6_test
+	sleep 60
+done
---
+++ scripts/perfAPITestScript.js
@@ -0,0 +1,19 @@
+/* eslint-disable prettier/prettier */
+/*
+Import all the necessary modules and libraries
+*/
+import http from 'k6/http';
+import { sleep } from 'k6';
+import { SharedArray } from 'k6/data';
+const Request = require('../projects/PerfTest/' + __ENV.Project + '/TestScripts/APIRequest.js');
+
+// console.log(`API Names are ${__ENV.APINAME}`);
+
+export let options = {
+    vus: __ENV.USERS,
+    iterations: __ENV.TESTINGTIME,
+}
+
+export default function () {
+    Request.frameAPIRequest(__ENV.APINAME);
+}
